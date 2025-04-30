8e81b4715e4643567da1bb73716d4467b271e53e
johanespelund <johan.espelund@hotmail.com>
2025-04-30 15:28:57 +0200

    Update plotting

--- plotVelocityProfile.py
+++ plotVelocityProfile.py
@@ -1,5 +1,4 @@
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import scienceplots
@@ -12,259 +11,176 @@ from FoamUtils import wall_profiles as wp
 from FoamUtils.ThermophysicalProperties import ThermophysicalProperties
 
 from data import Ampofo
-
-Ampofo = Ampofo.Ampofo()
-
+from data import DNS
 from functions import time_average
 
-folders = [
-    # "postProcessing",
-    # "./cases/test/2025-03-25_Ampofo_kOmegaSST_W0.0005_B0.01_BtsFalse_LTS-True/postProcessing",
-    # "./cases/test/2025-03-25_Ampofo_laminar_W0.0005_B0.01_BtsFalse_LTS-True/postProcessing",
-    # "./cases/bSource-GGDH/2025-03-25_Ampofo_kOmegaSST_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
+# Load reference data
+Ampofo = Ampofo.Ampofo()
+dns = DNS.DNS("db_1e10_lin")
+dns_Y0p5 = dns.load_data("Basic_stat/Basic_stat_X_0p5.dat")
+dns_X0p5 = dns.load_data("Data_midwidth/Data_midwidth.dat")
+dns_Nu_cold = dns.load_data("Nu/Nu_cold.dat")
+dns_Nu_hot = dns.load_data("Nu/Nu_hot.dat")
 
-    # "./cases/no_bSource_transient/2025-03-25_Ampofo_LaunderSharmaKE_W0.0005_B0.01_BtsFalse_LTS-False_maxCo0.1/postProcessing",
-    # "./cases/no_bSource_transient/2025-03-25_Ampofo_kOmegaSST_W0.0005_B0.01_BtsFalse_LTS-False_maxCo0.1/postProcessing",
-    # "./cases/no_bSource_transient/2025-03-25_Ampofo_v2f_W0.0005_B0.01_BtsFalse_LTS-False_maxCo0.1/postProcessing",
-
-    "./cases/bSource-SGDH/2025-03-25_Ampofo_v2f_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-SGDH/2025-03-25_Ampofo_kOmegaSST_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-SGDH/2025-03-25_Ampofo_kOmega_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-SGDH/2025-03-25_Ampofo_LaunderSharmaKE_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-
-    "./cases/bSource-GGDH/2025-03-25_Ampofo_v2f_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-GGDH/2025-03-25_Ampofo_kOmegaSST_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-GGDH/2025-03-25_Ampofo_kOmega_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-    "./cases/bSource-GGDH/2025-03-25_Ampofo_LaunderSharmaKE_W0.0005_B0.01_BtsTrue_LTS-True/postProcessing",
-
-    # "./cases/no_bSource_transient/2025-03-25_Ampofo_LaunderSharmaKE_W0.0005_B0.01_BtsFalse_LTS-False_maxCo0.1/postProcessing",
-    # "./cases/kOmegaSST/0.5cm0.3mm_noSource/postProcessing",
-    # "./cases/kOmegaSST/1cm0.3mm_noSource/postProcessing",
-    # "./cases/kOmegaSST/2cm0.3mm_noSource/postProcessing",
-]
-
-labels = [
-    "v2f", "kOmegaSST", "kOmega", "LSKE",
-    "v2f-G", "kOmegaSST-G", "kOmega-G", "LSKE-G",
-]
-
-linestyles = ["-"]*4 + ["--"]*4
-colors = [f"C{i}" for i in range(4)] * 2
-
+folders = ["postProcessing"]
+labels = ["$v^2-f$"]
+linestyles = ["-"]
+colors = ["C0"]
 thermo = ThermophysicalProperties("constant/thermophysicalProperties")
 
-# Read the n last velocity profiles
+# Standard figure settings
+figsize = (3.5, 2.5)  # inches
+fontsize = 9
+plt.rcParams.update({
+    "font.size": fontsize,
+    "axes.labelsize": fontsize,
+    "axes.titlesize": fontsize,
+    "legend.fontsize": fontsize,
+    "xtick.labelsize": fontsize,
+    "ytick.labelsize": fontsize,
+    "figure.dpi": 300,
+})
 
-marker = "none"
-fig, ax = plt.subplots(2, 3, figsize=(9, 4))
-ax = ax.flatten()
-i = 0
-for postProcessing in folders:
+for i, postProcessing in enumerate(folders):
     parameters = fu.read_parameters(f"{postProcessing}/../parameters")
-    print(f"turbulence model: {parameters['RASModel']}")
     Th, Tc = parameters["T_left"], parameters["T_right"]
     DeltaT = Th - Tc
     T0 = (Th + Tc) / 2
     L = parameters["L_x"]
-    V0 = np.sqrt(9.81 * L * (Th - Tc) / T0)
-    print(f"Plotting {postProcessing}")
-    label = labels[i] #postProcessing.split("/")[0]
-    sorted_folders = fu.get_sorted_times(f"{postProcessing}/linesample")
-    print(f"{sorted_folders=}")
-    time = sorted_folders[-1]
-    print(f"{time=}")
-    pp_color = colors[i]
+    Ra = parameters["Ra"]
+
+    alpha = thermo.alpha(1e5, T0)
+    uNorm = alpha * np.sqrt(Ra) / L
+    kNorm = uNorm**2
+    tNorm = (L**2) * (Ra**-0.5)/alpha
+
+    label = labels[i]
+    color = colors[i]
     ls = linestyles[i]
-    i += 1
+    time = fu.get_sorted_times(f"{postProcessing}/linesample")[-1]
 
     vertical = fu.load_sampling_set(f"{postProcessing}/linesample", time, "vertical", org=True)
-    horizontal = fu.load_sampling_set(
-        f"{postProcessing}/linesample", time, "horizontal", org=True)
-    # right = fu.load_sampling_set(f"{postProcessing}/wallsample", time, "rightwall", org=True)
-    # left = fu.load_sampling_set(f"{postProcessing}/wallsample", time, "leftwall", org=True)
-
+    horizontal = fu.load_sampling_set(f"{postProcessing}/linesample", time, "horizontal", org=True)
     right = fu.load(f"{postProcessing}/right_wall/{time}/line.xy")
     left = fu.load(f"{postProcessing}/left_wall/{time}/line.xy")
-    print(left)
-    # q_right = right[:,1]
-    # q_left = left[:,1]
 
-    z = vertical["z"]  # vertical[:, 0]
-    z_right = right["z"]
+    z = vertical["z"]
+    x = horizontal["x"]
+    T_vert = vertical["T"]
+    Ux_vert = vertical["Ux"]
+    Uz_hor = horizontal["UMeanz"]
+    T_hor = horizontal["TMean"]
+    k_hor = horizontal["kMean"] if parameters["RASModel"] != "laminar" else x * 0
+
     z_left = left["z"]
-    T_vert = vertical["T"]  # [:, iT]
-    Ux_vert = vertical["Ux"]  # [:, iUx]
-    Uz_vert = vertical["Ux"]  #:, iUz]
+    q_left = left["wallHeatFluxM..."]
+    z_right = right["z"]
+    q_right = right["wallHeatFluxM..."]
+    Nu_left = wp.Nusselt(q_left, DeltaT, L, thermo.kappa(Th))
+    Nu_right = wp.Nusselt(q_right, DeltaT, L, thermo.kappa(Th))
 
-    q_right = right["wallHeatFluxM..."]  # [:, 1]
-    q_left = left["wallHeatFluxM..."]  # [:, 1
+    # Ux vs z
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(Ux_vert, z/L, label=label, color=color, ls=ls)
+    ax.plot(dns_X0p5["V"], dns_X0p5["x"], **dns.plot_params)
+    ax.set_xlabel("$u/u^*$")
+    ax.set_ylabel("$y/y^*$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_Ux_vs_z.pdf")
 
-    Nu_left = wp.Nusselt(q_left, Th - Tc, L, thermo.kappa(Th))
-    Nu_right = wp.Nusselt(q_right, Th - Tc, L, thermo.kappa(Tc))
+    # T vs z
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot((T_vert - T0)/DeltaT, z/L, label=label, color=color, ls=ls)
+    ax.plot(dns_X0p5["T"], dns_X0p5["x"], **dns.plot_params)
+    ax.set_xlabel("$\Phi$")
+    ax.set_ylabel("$y/y*$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_T_vs_z.pdf")
 
-    T_hor = horizontal["TMean"]  # [:, 1]
-    Uz_hor = horizontal["UMeanz"]  # [:, iUz]
+    # Nu_hot vs z
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(Nu_left, z_left/L, label=label, color=color, ls=ls)
+    ax.plot(-dns_Nu_hot["Nu_hot"], dns_Nu_hot["x"], **dns.plot_params)
+    ax.set_xlabel("Nu$_\\text{hot}$")
+    ax.set_ylabel("$y/y^*$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_Nu_vs_z.pdf")
 
-    x = horizontal["x"]  # [:, 0]
-    if parameters["RASModel"] != "laminar":
-        k_hor = horizontal["kMean"]  # [:, 1
-    else:
-        k_hor = x*0
+    # Nu_cold vs z
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(Nu_right, 1 - z_right/L, label=label, color=color, ls=ls)
+    ax.plot(-dns_Nu_cold["Nu_cold"], dns_Nu_cold["x"], **dns.plot_params)
+    ax.set_xlabel("Nu$_\\text{cold}$")
+    ax.set_ylabel("$y/y^*$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_NuCold_vs_z.pdf")
+    # Uz vs x
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(x/L, Uz_hor/uNorm, label=label, color=color, ls=ls)
+    ax.plot(dns_Y0p5["y"], dns_Y0p5["U"], **dns.plot_params)
+    ax.set_xlabel("$x/x^*$")
+    ax.set_ylabel("$v/u^* $")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_Uz_vs_x.pdf")
 
-    Uz_left = horizontal["Uz"]  # [:, iUz]
-    Uz_right = np.flip(Uz_left)
+    # T vs x
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(x/L, (T_hor - T0)/DeltaT, label=label, color=color, ls=ls)
+    ax.plot(dns_Y0p5["y"], dns_Y0p5["T"], **dns.plot_params)
+    ax.set_xlabel("$x/x^*$ ")
+    ax.set_ylabel("$\Phi$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_T_vs_x.pdf")
 
-    rho_data = horizontal["rho"]
+    # k vs x
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(x/L, k_hor/kNorm, label=label, color=color, ls=ls)
+    ax.plot(dns_Y0p5["y"], dns_Y0p5["k"], **dns.plot_params)
+    ax.set_xlabel("$x/x^*$")
+    ax.set_ylabel("$k / u^{*2}$")
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig(f"{label}_k_vs_x.pdf")
 
-    n_left = horizontal["x"]
-    n_right = L - np.flip(n_left)
-    rho_left = rho_data[0]
-    # rho_right = rho_data[-1]
-    rho_right = rho_data.to_numpy()[-1]
 
-    T_left = T_hor
-    T_right = np.flip(T_hor)
+    # Nu vs time
+    fig, ax = plt.subplots(figsize=figsize)
 
-    uPlus_left = wp.u_plus(Uz_left, T_left, n_left, rho_left, thermo)
-    uPlus_right = wp.u_plus(Uz_right, T_right, n_right, rho_right, thermo)
+    data = fu.load_all_times(f"{postProcessing}/Q_left", "surfaceFieldValue.dat")
+    t, Q_left = data["Time"].to_numpy()/tNorm, data["areaIntegrate(wallHeatFlux)"]
+    Nu_left = Q_left / L / ((Th - Tc) * thermo.kappa(T0))
+    ax.plot(t, Nu_left, label=f"$Nu_h$ {labels[i]}", color=color)
 
-    nPlus_left = wp.n_plus(Uz_left, T_left, n_left, rho_left, thermo)
-    nPlus_right = wp.n_plus(Uz_right, T_right, n_right, rho_right, thermo)
+    t_mask = t > 60
+    t1, t2 = t[t_mask][0], t[t_mask][-1]
+    dt = t2 - t1
+    Nu_hot_mean = np.trapezoid(Nu_left[t_mask], t[t_mask])/dt
 
-    Tref = 0.5 * (Th + Tc)
-    phiPlus_left = wp.phi_plus(Uz_left, T_left, n_left, rho_left, Tref, thermo)
-    phiPlus_right = wp.phi_plus(Uz_left, T_right, n_right, rho_right, Tref, thermo)
+    print(f"Nu_hot_mean: {Nu_hot_mean: .2f}")
 
-    ax[0].plot(Ux_vert, z, label=label, marker=marker, markevery=1, color=pp_color, ls=ls)
+    data = fu.load_all_times(f"{postProcessing}/Q_right", "surfaceFieldValue.dat")
+    t, Q_right = data["Time"].to_numpy()/tNorm, data["areaIntegrate(wallHeatFlux)"]
+    Nu_right = -Q_right / L / ((Th - Tc) * thermo.kappa(T0))
+    ax.plot(t, Nu_right, label=f"$Nu_c$ {labels[i]}", color=color, ls="--")
 
-    ax[1].plot(T_vert, z, label=label, marker=marker, markevery=1, color=pp_color, ls=ls)
+    t_mask = t > 60
+    t1, t2 = t[t_mask][0], t[t_mask][-1]
+    dt = t2 - t1
+    Nu_cold_mean = np.trapezoid(Nu_right[t_mask], t[t_mask])/dt
+    print(f"Nu_cold_mean: {Nu_cold_mean: .2f}")
 
-    Nu_left = q_left * L / ((Th - Tc) * thermo.kappa(Th))
-    Nu_left_mean = np.trapz(Nu_left, z_left) / L
-    print(f"{label} Nu_left_mean = {Nu_left_mean}")
-    ax[2].plot(
-        Nu_left,
-        # q_left,
-        z_left / L,
-        label="Left wall",
-        color=pp_color, ls=ls
-    )
-    ax[2].plot(
-        Nu_right,
-        1 - (z_right / L),
-        label="Right wall",
-        color=pp_color, ls=ls
-    )
 
-    ax[3].plot(x / L, Uz_hor / V0, label=label, marker=marker, markevery=1, color=pp_color, ls=ls)
 
-    ax[4].plot(
-        x / L,
-        (T_hor - Tc) / DeltaT,
-        label=label,
-        marker=marker,
-        markevery=1,
-        color=pp_color,
-        ls=ls
-    )
 
-    ax[5].plot(
-        x / L, k_hor / V0**2, label=label, marker=marker, markevery=1, color=pp_color, ls=ls
-    )
-
-ax[0].set_ylabel("z/L [-]")
-ax[0].set_xlabel("Ux [m/s]")
-
-ax[1].set_ylabel("z/L [-]")
-ax[1].set_xlabel("T [K]")
-
-ax[2].plot(Ampofo.Nu_hot[:, 1], Ampofo.Nu_hot[:, 0], "xk", label=Ampofo.label + " left")
-ax[2].plot(
-    Ampofo.Nu_cold[:, 1], Ampofo.Nu_cold[:, 0], "ok", label=Ampofo.label + " right"
-)
-ax[2].set_ylabel("z/L [-]")
-ax[2].set_xlabel("Nu [-]")
-ax[2].legend()
-
-ax[3].plot(Ampofo.hor_mid["X"], Ampofo.hor_mid["vBar/V0"], "ko", label=Ampofo.label)
-ax[3].set_ylabel("Uz [m/s]")
-ax[3].set_xlabel("x/L [-]")
-ax[3].legend()
-
-ax[4].plot(
-    Ampofo.hor_mid["X"], Ampofo.hor_mid["(TBar-Tc)/DeltaT"], "ko", label=Ampofo.label
-)
-ax[4].set_ylabel("T [K]")
-ax[4].set_xlabel("x/L [-]")
-ax[4].legend()
-
-ax[5].plot(Ampofo.hor_mid["X"], Ampofo.hor_mid["k/V0^2"], "ko", label=Ampofo.label)
-ax[5].set_ylabel("k [m2/s2]")
-ax[5].set_xlabel("x/L [-]")
-ax[5].legend()
-
-lines = [
-    plt.Line2D([0], [0], color=f"C{i}", linewidth=2, linestyle="-", label=f"{time}")
-    for i, postProcessing in enumerate(folders)
-]
-fig.legend(
-    handles=lines,
-    loc="upper center",
-    ncol=2,
-    frameon=True,
-)
+ax.set_xlabel("$t/t^*$")
+ax.set_ylabel("Nu")
+ax.legend(ncol=2)
 fig.tight_layout()
-plt.figure()
-
-k = 0
-for ppDir in folders:
-    pp_color = f"C{k}"
-    k += 1
-    # t, whf = fu.load_wallHeatFlux(f"{ppDir}/wallHeatFlux/2100/wallHeatFlux.dat", start=100)
-    data = fu.load_all_times(f"{ppDir}/Q_left", "surfaceFieldValue.dat")
-    t, Q_left = data["Time"], data["areaIntegrate(wallHeatFlux)"]
-    Nu_left = Q_left / L / ((Th - Tc) * thermo.kappa(Th))
-    plt.plot(t, Nu_left, label=f"$Nu_h$ {ppDir}", color=pp_color)
-
-    data = fu.load_all_times(f"{ppDir}/Q_right", "surfaceFieldValue.dat")
-    t, Q_right = data["Time"], data["areaIntegrate(wallHeatFlux)"]
-    Nu_right = -Q_right / L / ((Th - Tc) * thermo.kappa(Tc))
-    plt.plot(t, Nu_right, label=f"$Nu_c$ {ppDir}", color=pp_color, ls="--")
-
-    # plt.plot(t, -whf["wall_right"], label=f"$Q_c$ {ppDir}", color=pp_color)
-    # plt.plot(t, whf["wall_left"], label=f"$Q_h$ {ppDir}")
-
-#     peaks, _ = sp.signal.find_peaks(-whf["wall_right"], distance=1000)
-#     i_start, i_end = peaks[-10], peaks[-1]
-#     t_start, t_end = t[i_start], t[i_end]
-#     T = t_end - t_start
-#     Qc = np.trapz(-whf["wall_right"][i_start:i_end], t[i_start:i_end]) / T
-#     plt.axhline(Qc, color="k", ls="--", label=f"$Q_c$ {ppDir} average")
-#     # print(f"Averageing right wall from {t_start} to {t_end}")
-
-#     # Same for left wall
-#     peaks, _ = sp.signal.find_peaks(whf["wall_left"], distance=1000)
-#     i_start, i_end = peaks[-10], peaks[-1]
-#     t_start, t_end = t[i_start], t[i_end]
-#     T = t_end - t_start
-#     Qh = np.trapz(whf["wall_left"][i_start:i_end], t[i_start:i_end]) / T
-#     plt.axhline(Qh, color="k", ls=":", label=f"$Q_h$ {ppDir} average")
-#     # print(f"Averageing left wall from {t_start} to {t_end}")
-
-#     plt.xlabel("t [s]")
-#     plt.ylabel("Q [W/m2]")
-#     plt.legend()
-
-#     # print(f"{ppDir} Qc = {Qc}")
-#     # print(f"{ppDir} Qh = {Qh}")
-
-    # Nu_c = wp.Nusselt(Qc, Th - Tc, L, thermo.kappa(T0))
-#     Nu_h = wp.Nusselt(Qh, Th - Tc, L, thermo.kappa(T0))
-#     Nu_avg = (Nu_c + Nu_h) / 2
-#     # print(f"{ppDir} Nu_c = {Nu_c}")
-#     # print(f"{ppDir} Nu_h = {Nu_h}")
-#     # print(f"{ppDir} Nu_avg = {Nu_avg}")
-
-
-plt.tight_layout()
+fig.savefig("Nu_vs_t.pdf")
 plt.show()
